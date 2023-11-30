b0e72c7a379bad8905b7c5f8ba98a26533aafa43
Torben Clasen <t.clasen@outlook.com>
2023-11-30 20:05:22 +0100

    Add -1/error GET 500

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn respond_internal_server_error() -> (StatusCode, &'static str) {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/error", get(respond_internal_server_error));
 
     Ok(router.into())
 }
