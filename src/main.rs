7e8159b3f641be50adccecf0b579cb79023366a4
Torben Clasen <t.clasen@outlook.com>
2023-12-03 15:45:35 +0100

    day 1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
 use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
 
+async fn pow_three(Path(num1): Path<i32>) -> impl IntoResponse {
+    num1.pow(3).to_string()
+}
+
 async fn xor_pow_three(Path((num1, num2)): Path<(i32, i32)>) -> impl IntoResponse {
     (num1 ^ num2).pow(3).to_string()
+}
+
+async fn sled_id(Path((num1, num2, rest)): Path<(i32, i32, String)>) -> impl IntoResponse {
+    let mut result = vec![num1, num2];
+    if rest.contains('/') {
+        result.append(
+            &mut rest
+                .split('/')
+                .map(|rest_path| {
+                    rest_path
+                        .parse::<i32>()
+                        .expect("Should be parsable into i32!")
+                })
+                .collect::<Vec<i32>>(),
+        );
+    } else {
+        result.push(rest.parse::<i32>().expect("Should be parsable into i32!"))
+    }
+    let result = result
+        .into_iter()
+        .reduce(|acc, x| acc ^ x)
+        .expect("Should be reducable!");
+    result.pow(3).to_string()
 }
 
 async fn respond_internal_server_error() -> (StatusCode, &'static str) {
@@ -11,7 +38,9 @@ async fn respond_internal_server_error() -> (StatusCode, &'static str) {
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
+        .route("/1/:num1", get(pow_three))
         .route("/1/:num1/:num2", get(xor_pow_three))
+        .route("/1/:num1/:num2/*rest", get(sled_id))
         .route("/-1/error", get(respond_internal_server_error));
 
     Ok(router.into())
