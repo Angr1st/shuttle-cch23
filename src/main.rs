a2b3bfc40f5c3316e28c5efd74711765c0bc2953
Torben Clasen <t.clasen@outlook.com>
2023-12-05 22:57:18 +0100

    day 4 task 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::Deserialize;
 
 async fn pow_three(Path(num1): Path<i32>) -> impl IntoResponse {
     num1.pow(3).to_string()
@@ -35,12 +42,27 @@ async fn respond_internal_server_error() -> (StatusCode, &'static str) {
     (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
 }
 
+#[derive(Deserialize)]
+struct Reindeer {
+    name: String,
+    strength: u32,
+}
+
+async fn reindeer_cheer(Json(reindeers): Json<Vec<Reindeer>>) -> impl IntoResponse {
+    reindeers
+        .into_iter()
+        .map(|deer| deer.strength)
+        .sum::<u32>()
+        .to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/1/:num1", get(pow_three))
         .route("/1/:num1/:num2", get(xor_pow_three))
         .route("/1/:num1/:num2/*rest", get(sled_id))
+        .route("/4/strength", post(reindeer_cheer))
         .route("/-1/error", get(respond_internal_server_error));
 
     Ok(router.into())
