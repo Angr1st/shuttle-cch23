05615a71d12859e326b8bc1805749a3c6e3838ab
Torben Clasen <t.clasen@outlook.com>
2023-12-07 21:29:18 +0100

    day 4 task 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
 use axum::{
     extract::Path,
     http::StatusCode,
-    response::IntoResponse,
+    response::{IntoResponse, Response},
     routing::{get, post},
     Json, Router,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+
+struct AppError {}
+
+impl AppError {
+    fn new() -> Self {
+        AppError {}
+    }
+}
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
+    }
+}
+
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
+//impl<E> From<E> for AppError {
+//    fn from(_: E) -> Self {
+//        AppError {}
+//    }
+//}
 
 async fn pow_three(Path(num1): Path<i32>) -> impl IntoResponse {
     num1.pow(3).to_string()
@@ -56,6 +79,68 @@ async fn reindeer_cheer(Json(reindeers): Json<Vec<Reindeer>>) -> impl IntoRespon
         .to_string()
 }
 
+#[derive(Deserialize)]
+struct ReindeerDetails {
+    name: String,
+    strength: u32,
+    speed: f64,
+    height: u32,
+    antler_width: u32,
+    snow_magic_power: u32,
+    favorite_food: String,
+    #[serde(rename = "cAnD13s_3ATeN-yesT3rdAy")]
+    candies_eaten_yesterday: u32,
+}
+
+#[derive(Serialize)]
+struct ReindeerChallengeResponse {
+    fastest: String,
+    tallest: String,
+    magician: String,
+    consumer: String,
+}
+
+async fn cursed_candy_eating_contest(
+    Json(reindeers): Json<Vec<ReindeerDetails>>,
+) -> Result<Json<ReindeerChallengeResponse>, AppError> {
+    let fastest = reindeers
+        .iter()
+        .map(|deer| {
+            (deer, unsafe {
+                (deer.speed * 10 as f64).to_int_unchecked::<u32>()
+            })
+        })
+        .max_by_key(|deer| deer.1)
+        .ok_or(AppError::new())?;
+    let tallest = reindeers
+        .iter()
+        .max_by_key(|deer| deer.height)
+        .ok_or(AppError::new())?;
+    let magician = reindeers
+        .iter()
+        .max_by_key(|deer| deer.snow_magic_power)
+        .ok_or(AppError::new())?;
+    let consumer = reindeers
+        .iter()
+        .max_by_key(|deer| deer.candies_eaten_yesterday)
+        .ok_or(AppError::new())?;
+    Ok(Json::from(ReindeerChallengeResponse {
+        fastest: format!(
+            "Speeding past the finish line with a strength of {} is {}",
+            fastest.0.strength, fastest.0.name
+        ),
+        tallest: format!(
+            "{} is standing tall with his {} cm wide antlers",
+            tallest.name, tallest.antler_width
+        ),
+        magician: format!(
+            "{} could blast you away with a snow magic power of {}",
+            magician.name, magician.snow_magic_power
+        ),
+        consumer: format!("{} ate lots of candies, but also some grass", consumer.name),
+    }))
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
@@ -63,6 +148,7 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .route("/1/:num1/:num2", get(xor_pow_three))
         .route("/1/:num1/:num2/*rest", get(sled_id))
         .route("/4/strength", post(reindeer_cheer))
+        .route("/4/contest", post(cursed_candy_eating_contest))
         .route("/-1/error", get(respond_internal_server_error));
 
     Ok(router.into())
